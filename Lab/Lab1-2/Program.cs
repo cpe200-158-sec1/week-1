aff537c0418b77b2b354abaec67192c9fe02ac0c
kyuuuyki <chayanon_ardkham@cmu.ac.th>
2015-08-22 15:55:00 +0700

    Get in put from command line

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -8,6 +8,7 @@ namespace Lab1_2
 {
     class BubbleSortFunction
     {
+        /*
         static void Main(string[] args)
         {
             bool flag = true;
@@ -38,6 +39,50 @@ namespace Lab1_2
                 Console.Write(" ");
             }
             Console.ReadKey();
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            int[] input = inp();
+            outp(proc(input));
+            Console.ReadKey();
+        }
+
+        static int[] inp()
+        {
+            string recieve = Console.ReadLine();
+            int[] input = recieve.Split(' ').Select(integer => Convert.ToInt32(integer)).ToArray();
+            return input;
+        }
+
+        static int[] proc(int[] input)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i != input.Length - 1; i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+            return input;
+        }
+
+        static void outp(int[] input)
+        {
+            for (int i = 0; i != input.Length; i++)
+            {
+                Console.Write(input[i]);
+                Console.Write(" ");
+            }
         }
     }
 }

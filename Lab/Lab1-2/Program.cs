10d53be86d954fe133ae55ee1730ecc24361bee4
jenb0111 <jenniejenny0111@gmail.com>
2015-08-23 22:07:50 +0700

    570610587

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -51,7 +51,7 @@ namespace Lab1_2
         static int[] inp()
         {
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-            return input;
+            return input ;
         }
 
         static int[] process(int[] input)

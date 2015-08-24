74574a54c9fc46c13369270456d69fadd39a0612
BosESouNDZA <bose_056@hotmail.com>
2015-08-24 16:48:33 +0700

    570610578 1-2 commit1

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,34 +10,42 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
-            bool flag = true;
+            int[] num = inputfunction();
+            processfunction(num);
+            outputfunction(num);
+            Console.ReadKey();
 
-            // input
+        }
+        static int[] inputfunction()
+        {
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
+            return input;
+        }
+        static void processfunction(int[] x)
+        {
+            bool flag = true;
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                for (int i = 0; i != x.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (x[i] > x[i + 1])
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                        int temp = x[i];
+                        x[i] = x[i + 1];
+                        x[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
-
-            // output
-            for (int i = 0; i != input.Length; i++)
+        }
+        static void outputfunction(int[] x)
+        {
+            for (int i = 0; i != x.Length; i++)
             {
-                Console.Write(input[i]);
+                Console.Write(x[i]);
                 Console.Write(" ");
             }
-            Console.ReadKey();
         }
     }
 }

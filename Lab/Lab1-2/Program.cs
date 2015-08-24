210b92e8754bc46f8bf783c9e472a97d4af1ba0b
KRIANGKRAI KOLIBUT <kriangkai_k@cmu.ac.th>
2015-08-24 19:23:14 +0700

    LAB 1-2 PATH 1 (Three Function)

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -8,7 +8,7 @@ namespace Lab1_2
 {
     class BubbleSortFunction
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
             output(process(input()));
             Console.ReadKey();
@@ -16,14 +16,14 @@ namespace Lab1_2
 
         static int[] input()
         {
-            string get = Console.ReadLine();
-            int[] input = get.Split(' ').Select(int => Convert.ToInt32(int)).ToArray();
+            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            return input;
         }
-
+                
         static int[] process(int[] input)
         {
             bool flag = true;
-      
+
             while (flag)
             {
                 flag = false;

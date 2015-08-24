0e22aa9ff82a6ebfa2444252277ad99b9d3a41d6
trewzaki <tongtananut@gmail.com>
2015-08-24 17:07:42 +0700

    570610574 Lab 1-2 Three Function

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,34 +10,47 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
+            int[] input = new int[10];
+            input_Num(input);
+            process(input);
+            output(input);
+
+            Console.ReadKey();
+        }
+
+        static void input_Num(int[] _input)
+        {
+            int[] input = { 4 , 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            for (int i = 0; i != input.Length; i++)
+                _input[i] = input[i];
+        }
+
+        static void process(int[] _input)
+        {
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                for (int i = 0; i != _input.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (_input[i] > _input[i + 1])
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                        int temp = _input[i];
+                        _input[i] = _input[i + 1];
+                        _input[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
+        }
 
-            // output
-            for (int i = 0; i != input.Length; i++)
+        static void output(int[] _input)
+        {
+            for (int i = 0; i != _input.Length; i++)
             {
-                Console.Write(input[i]);
+                Console.Write(_input[i]);
                 Console.Write(" ");
             }
-            Console.ReadKey();
         }
     }
 }
